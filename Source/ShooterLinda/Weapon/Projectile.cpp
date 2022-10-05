2599663bfd7eb15a9ac9b35a9ba55d4a1a9f91d2
Raushan <resh87@mail.ru>
2022-10-05 19:08:40 +0600

    added particles to the projectiles

--- Source/ShooterLinda/Weapon/Projectile.cpp
+++ Source/ShooterLinda/Weapon/Projectile.cpp
@@ -2,8 +2,11 @@
 
 
 #include "Projectile.h"
- #include "Components/BoxComponent.h"
- #include "GameFramework/ProjectileMovementComponent.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Particles/ParticleSystem.h"
 AProjectile::AProjectile()
 { 
 	PrimaryActorTick.bCanEverTick = true;
@@ -22,7 +25,18 @@ AProjectile::AProjectile()
 void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if(Tracer)
+	{
+		TracerComponent = UGameplayStatics::SpawnEmitterAttached(
+			Tracer,
+			CollisionBox,
+			FName(),
+			GetActorLocation(),
+			GetActorRotation(),
+			EAttachLocation::KeepWorldPosition
+		);
+	}
 }
  
 void AProjectile::Tick(float DeltaTime)
